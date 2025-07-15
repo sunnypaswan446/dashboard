53b677e1b9e4cc78cb2d9cb825209cabc602c5ee
Sunny Paswan <paswansunny446@gmail.com>
2025-07-15 23:16:08 +0530

    Created notification block

---
+++ src/Components/Notification/index.tsx
@@ -0,0 +1,120 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <>
+      <div className="notify">
+        <div className="notify__cont">
+          <div className="notify__head">
+            <h5 className="notify__heading">Notification</h5>
+            <div className="notify__close">
+              <img
+                src="../images/close-icon.svg"
+                className="notify__img"
+                alt="close"
+              />
+            </div>
+          </div>
+          <div className="notify__wrap">
+            <a href="" className="notify__item notify__item--unread">
+              <div className="cta__text notify__top">
+                <p className="notify__title">
+                  The company is pivoting to a new market
+                </p>
+                <span className="notify__tag">Unread</span>
+              </div>
+              <div className="notify__bottom text__small ">
+                <p className="notify__content">
+                  More description goes here and it ca nbe very...
+                </p>
+                <span className="notify__time">4.4.2019; 13:44</span>
+              </div>
+            </a>
+            <a href="" className="notify__item">
+              <div className="cta__text notify__top">
+                <p className="notify__title">
+                  The company is pivoting to a new market
+                </p>
+              </div>
+              <div className="notify__bottom text__small ">
+                <p className="notify__content">
+                  More description goes here and it ca nbe very...
+                </p>
+                <span className="notify__time">4.4.2019; 13:44</span>
+              </div>
+            </a>
+            <a href="" className="notify__item notify__item--unread">
+              <div className="cta__text notify__top">
+                <p className="notify__title">
+                  The company is pivoting to a new market
+                </p>
+                <span className="notify__tag">Unread</span>
+              </div>
+              <div className="notify__bottom text__small ">
+                <p className="notify__content">
+                  More description goes here and it ca nbe very...
+                </p>
+                <span className="notify__time">4.4.2019; 13:44</span>
+              </div>
+            </a>
+            <a href="" className="notify__item">
+              <div className="cta__text notify__top">
+                <p className="notify__title">
+                  The company is pivoting to a new market
+                </p>
+              </div>
+              <div className="notify__bottom text__small ">
+                <p className="notify__content">
+                  More description goes here and it ca nbe very...
+                </p>
+                <span className="notify__time">4.4.2019; 13:44</span>
+              </div>
+            </a>
+            <a href="" className="notify__item notify__item--unread">
+              <div className="cta__text notify__top">
+                <p className="notify__title">
+                  The company is pivoting to a new market
+                </p>
+                <span className="notify__tag">Unread</span>
+              </div>
+              <div className="notify__bottom text__small ">
+                <p className="notify__content">
+                  More description goes here and it ca nbe very...
+                </p>
+                <span className="notify__time">4.4.2019; 13:44</span>
+              </div>
+            </a>
+            <a href="" className="notify__item">
+              <div className="cta__text notify__top">
+                <p className="notify__title">
+                  The company is pivoting to a new market
+                </p>
+              </div>
+              <div className="notify__bottom text__small ">
+                <p className="notify__content">
+                  More description goes here and it ca nbe very...
+                </p>
+                <span className="notify__time">4.4.2019; 13:44</span>
+              </div>
+            </a>
+            <a href="" className="notify__item">
+              <div className="cta__text notify__top">
+                <p className="notify__title">
+                  The company is pivoting to a new market
+                </p>
+              </div>
+              <div className="notify__bottom text__small ">
+                <p className="notify__content">
+                  More description goes here and it ca nbe very...
+                </p>
+                <span className="notify__time">4.4.2019; 13:44</span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default index;
