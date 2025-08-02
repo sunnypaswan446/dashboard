b8c5334c8ac0c94d592321af914ea4a94249819f
Sunny Paswan <paswansunny446@gmail.com>
2025-08-02 22:33:36 +0530

    Update server ans ssl page sub menus paths

--- src/pages/SSL/index.tsx
+++ src/pages/SSL/index.tsx
@@ -1,16 +1,11 @@
-import Aside from "../../Components/Aside";
-import Header from "../../Components/Header";
 import ContentArea from "../../Components/ContentArea";
-import { menus, sslMenu } from "../../mock-data/data";
+import { Outlet } from "react-router-dom";
 
 const index = () => {
   return (
     <>
-      <Header data={true} />
-      <div className="content">
-        <Aside data={menus} asideData={sslMenu} />
-        {/* <ContentArea /> */}
-      </div>
+      <ContentArea />
+      <Outlet />
     </>
   );
 };
